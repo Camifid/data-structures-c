5d0ab7dec37038ebb2eb60b2fc7ce1bcaa145a98
Camilli Fidelis <109491441+Camifid@users.noreply.github.com>
2023-08-03 12:10:53 -0300

    Update MyFirstFile.c

--- MyFirstFile.c
+++ MyFirstFile.c
@@ -5,7 +5,8 @@ float num = 2.3;
 double d = 4.6;
 char ch = 't';
 
-	int main(void) {
+int main(void) {
+
 printf("O valor da variável 'a' é: %d\n",a);
 printf("O valor da variável 'num' é: %.1f\n", num);	
 printf("O valor da variável 'd' é: %.1f\n", d);
