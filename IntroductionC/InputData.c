faa36015b74db1355c0b3a393da2bafa6280f753
Camilli Fidelis <109491441+Camifid@users.noreply.github.com>
2023-08-03 12:17:08 -0300

    Create InputData.c

---
+++ IntroductionC/InputData.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	char letra;
+	
+	printf("\nDigite uma letra: ");
+	scanf("%c", &letra);
+	printf("\nO valor da variável 'letra' é: %c. \n", letra);
+	
+  return 0;
+}
+
+/* 
+	float num, soma;
+	printf("\nDigite um número: ");
+	scanf("%f", &num);
+	soma = num + 5;
+	
+	printf("\nO valor da variável 'num' + 5 é: %.1f\n\n", soma);
+*/ 
